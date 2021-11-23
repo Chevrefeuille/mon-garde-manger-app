c61b33c1257c486ec3b465c95f75667c736f9e6c
Adrien Gregorj <adrien.gregorj@gmail.com>
2021-11-23 14:11:58 +0100

    configure prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<template><div>HelloWorld</div></template>
+<template>
+  <div>HelloWorld</div>
+</template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
 });
 </script>
 
