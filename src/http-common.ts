7cc45c7d94db2defe01a40a7333bb790fa91f305
Adrien Gregorj <adrien.gregorj@gmail.com>
2021-11-26 10:24:14 +0100

    use env variable for api url

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
